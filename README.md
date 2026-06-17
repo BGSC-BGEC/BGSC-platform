bf3638d0354009e903e9b75a0fa6c02ca5b27b95
Nikunj Goyal <nikunjgoyal1106@gmail.com>
2026-06-17 20:46:58 +0530

    Revert "Dev merge into main"

---
+++ README.md
@@ -0,0 +1,1 @@
+# BGSC-Application-Backend
--- backend/apps/auth-service/src/exceptions/account-disabled.exception.ts
+++
@@ -1,7 +0,0 @@
-import { ForbiddenException } from '@nestjs/common';
-
-export class AccountDisabledException extends ForbiddenException {
-  constructor(message = 'Account is disabled. Contact support.') {
-    super(message);
-  }
-}
